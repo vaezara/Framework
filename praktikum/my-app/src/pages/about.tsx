8630d4686bd78435481c1e464746fa90f4f7e717
Nova Eliza Maharani <novaelzrayya@gmail.com>
2026-02-13 20:10:33 +0700

    Jobsheet 1 - Tugas Praktikum 1

---
+++ praktikum/my-app/src/pages/about.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Tugas 1 Praktikum</h1>
+      <h3>Nama : Nova Eliza Maharani <br />
+        NIM : 2341720252 <br />
+        Program Studi : D-IV Teknik Informatika
+      </h3>
+    </div>
+  )
+}
