122022e3420409a50a28188ac0ef75e186e61f51
Nova Eliza Maharani <novaelzrayya@gmail.com>
2026-02-21 17:27:19 +0700

    Jobsheet 2 - Tugas Praktikum 1 (fix layout /profile)

--- praktikum/my-app-j02/src/pages/profile/index.tsx
+++ praktikum/my-app-j02/src/pages/profile/index.tsx
@@ -8,16 +8,28 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Profile() {
   return (
-    <div>
-      <h3>Profil Saya ^-^</h3>
+    <div className={styles.container}>
+      <h3>My Profile</h3>
       <br />
-      <p><strong>Nama:</strong> Nova Eliza Maharani</p>
-      <p><strong>Kelas:</strong> TI-3D</p>
-      <p><strong>Program Studi</strong> D4-Teknik Informatika</p>
-      <p><strong>Jurusan:</strong> Teknologi Informasi</p>
-      <br />
+
+      <div className={styles.formGroup}>
+        <p><strong>Nama:</strong> Nova Eliza Maharani</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Kelas:</strong> TI-3D</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Program Studi:</strong> D4-Teknik Informatika</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Jurusan:</strong> Teknologi Informasi</p>
+      </div>
+
       <Link href="/profile/edit">
-        <button>Edit Profile</button>
+        <button className={styles.button}>Edit Profile</button>
       </Link>
     </div>
   );
