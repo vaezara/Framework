13c173e4244096d79fdaec833714602cfa638d4e
Nova Eliza Maharani <novaelzrayya@gmail.com>
2026-03-09 07:45:25 +0700

    Jobsheet 9 - Langkah 1

---
+++ praktikum/my-app-j09/README.md
@@ -0,0 +1,28 @@
+## <p align = "center">LAPORAN PRAKTIKUM JOBSHEET 9</p>
+## <p align = "center">STATIC SITE GENERATION</p>
+
+<br><br>
+
+<p align="center">
+  <img src="public/img/logo_polinema.png" width="40%">
+</p>
+
+<br><br>
+
+## <p align = "center">Oleh :</p>
+## <p align = "center">Nova Eliza Maharani</p>
+## <p align = "center">NIM. 2341720252 </p>
+
+<br><br>
+
+## <p align = "center">PROGRAM STUDI D-IV TEKNIK INFORMATIKA</p>
+## <p align = "center">JURUSAN TEKNOLOGI INFORMASI</p>
+## <p align = "center">POLITEKNIK NEGERI MALANG</p>
+## <p align = "center">MARET 2026</p>
+
+<br><br>
+
+## Hasil Praktikum
+
+### Langkah 1 – Setup Halaman Static
+![1](public/img/1.png)
---
+++ praktikum/my-app-j09/src/pages/produk/static.tsx
@@ -0,0 +1,25 @@
+import TampilanProduk from "../views/product";
+import { ProductType } from "../types/Product.type";
+
+const halamanProdukStatic = (props:{products:ProductType[]}) => {
+    const { products } = props;
+    return (
+        <div>
+            <h1>Halaman Produk Static</h1>
+            <TampilanProduk products={products} />
+        </div>
+    );
+};
+export default halamanProdukStatic;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response: { data: ProductType[] } = await res.json();
+  // console.log("Data produk yang diambil dari API:", respone);
+  return {
+    props: {
+      products: response.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    }
+  }
+}
