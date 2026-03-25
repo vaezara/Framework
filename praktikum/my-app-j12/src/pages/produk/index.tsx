8eba5f620ecedae19a53afa1e2e6aa094ae4e8a1
Nova Eliza Maharani <novaelzrayya@gmail.com>
2026-03-25 14:35:51 +0700

    Jobsheet 12 - Langkah 1

---
+++ praktikum/my-app-j12/src/pages/produk/index.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../../views/product";
+import useSWR from "swr";
+import fetcher from "../../utils/swr/fetcher";
+//import { retrieveProducts } from "../../utils/db/servicefirebase";
+
+
+//const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+   // console.log("products:", products);
+
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  //cek apakah data, error, dan isLoading sudah benar
+  // console.log("data:", data);
+  // console.log("error:", error);
+  // console.log("isLoading:", isLoading);
+
+  return (
+    <div>
+      <TampilanProduk products={isLoading ? [] : data.data} />
+    </div>
+  );
+};
+
+export default kategori;
+
+// export async function getStaticProps() {
+//   const products = await retrieveProducts("products");
+
+//   return {
+//     props: {
+//       products,
+//     },
+//     revalidate: 10, // ISR setiap 10 detik
+//   };
+// }
